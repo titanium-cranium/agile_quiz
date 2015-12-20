6786ffc99c192b85179f4ae6b9180a8044f03ffe
titanium-cranium <titaniumcranium@icloud.com>
2015-12-20 03:39:31 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AgileQuiz::Application.config.secret_key_base = 'c97a42ecb73494338d4418711b040e19809591afbffa18030a0d1769ba494fbef452319dc48f387c89b282df7f8f6f78fb8c6744ae4b07fbe123b3774523792a'
